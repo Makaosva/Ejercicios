8382d6235caa8c6e7dd460a30ec6bba4ec73f5be
Makaosva <maka.osses@gmail.com>
2024-03-28 00:15:36 -0300

    feat/arreglo ejercicio 2

--- 2/assets/js/script.js
+++ 2/assets/js/script.js
@@ -1,5 +1,5 @@
-function Cambia(){
-    const color= '#'
-
+function Cambia() {
     const color= '#' + Math.floor(Math.random() * 16777215).toString(16); // Generamos un color hexadecimal aleatorio
+    cuerpo = document.querySelector('body');
+    cuerpo.style.backgroundColor = color;
 }
